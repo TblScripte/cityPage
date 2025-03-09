a63ab3f146233b6d2b965a1e64b2e72d36d1e309
TblScripte <tegnievbilol@gmail.com>
2025-03-09 21:01:31 +0500

    start

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import img1 from "../src/assets/uae-4-2048x1367\ 2\ \(1\).png"
+
+const App = () => {
+  return (
+    <div >
+      <div className='header_bgImg'>
+      </div>
+    </div>
+  )
+}
+
+export default App
